28e9c68166e9564e1c86a2846b728fa58fd4f41b
Alexcey <alexcey007@yandex.ru>
2020-06-09 01:12:06 +0300

    update user class

--- lib/user.rb
+++ lib/user.rb
@@ -1,28 +1,48 @@
 class User
   BANK = 100
   RATE = 10
+  MAX_CARDS = 3
+  BLACK_JACK = 21
 
-  attr_reader :name, :balance
+  attr_reader :name, :balance, :points, :cards
 
   def initialize(name)
     @name = name
     @balance = BANK
-    @card = []
+    @cards = []
+    @points = 0
+  end
+
+  def take_card(desk)
+    card = desk.take_card
+    ace_calculate if card.value == 1
+    @points += card.value
+    @cards << card
+  end
+
+  def can_take?
+    cards.length < MAX_CARDS
   end
 
   def can_play?
-    @balance >= RATE
+    balance >= RATE
   end
 
-  def loss_bank
+  def bet
     @balance -= RATE
   end
 
   def win_bank
-    @balance += RATE*2
+    @balance += RATE * 2
   end
 
   def clear
-    @card.clear
+    card.clear
+  end
+
+  private
+
+  def ace_calculate
+    @points += @points + 11 <= BLACK_JACK ? 11 : 1
   end
 end
